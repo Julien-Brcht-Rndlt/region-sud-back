7a149d69fee2ffbaf67e1a68f1b720c1b7f8d270
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-21 13:59:38 +0200

    settings of database connection

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.use(express.json());
 // TODO: configure cors
 app.use(cors());
 
+connection.connect((err) => {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+    } else {
+        console.log('connected as id ' + connection.threadId);
+    }
+});
+
 // TODO: add your routes here
 
 module.exports = app;
