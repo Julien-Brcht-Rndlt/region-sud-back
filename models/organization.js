152c7eef1c9439f8e3df85dfebf8512997438507
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-21 18:06:15 +0200

    adding more behaviour to organization Model + routes

--- models/organization.js
+++ models/organization.js
@@ -8,7 +8,41 @@ const find = (orgId) => {
     return;
 }
 
+const findByName = () => {
+    const sql = 'SELECT * FROM organization WHERE orgName = ?';
+    return connection.promise.query(sql,[orgName]).then(([results]) => {
+        return results;
+    });
+}
+
+const create = ({ orgName, orgStaff }) => {
+    const validationErrors = validation({ orgName, orgStaff });
+    if(validationErrors) return Promise.reject('INVALID_DATA');
+    findByName(orgName).then((result) => {
+        if(results.lenght) {
+            return Promise.reject('DUPLICATE_NAME');
+        }
+    });
+    const sql = 'INSERT INTO organization (orgName, orgStaff) VALUES (?, ?)';
+    connection.promise().query(sql,[orgName, orgStaff])
+                        .then((result) => {
+                            const id = result.insertId;
+                            return { id, orgName, orgStaff };
+                        });
+
+};
+
+const validation = ({ orgName, orgStaff }) => {
+    let validationErrors = null;
+    validationErrors = Joi.object({
+        orgName: Joi.string().email().max(255).required(),
+        orgStaff: Joi.number().orgStaff().max().required(), 
+      }).validate({ orgName, orgStaff }, { abortEarly: false }).error;
+    return validationErrors;
+}
+
 module.exports = {
     findAll,
     find,
+    create,
 }
--- routes/organizations.js
+++ routes/organizations.js
@@ -12,4 +12,23 @@ orgRouter.get('/:id', (req, res) => {
                 .catch((err) => res.status(500).send(`Error retrieving organization (#${orgId}): ${err.message}`));
 });
 
+orgRouter.post('/organization', (req, res) => {
+    const { orgName, orgStaff } = req.body;
+    Organization.create(orgId)
+                .then(([insertId]) => {
+                    res.status(201).json({id: insertId, ...req.body});
+                })
+                .catch((err) => {
+                    if (err === 'DUPLICATE_NAME') {
+                        res.status(409).json({ message: 'This organization name is already used'});
+                    }
+                    else if(err === 'INVALIDE_DATA') {
+                        res.status(422).json({/* validationError */})
+                    }
+                    else {
+                        res.status(500).send('Error saving the organization');
+                    } 
+                });
+});
+
 module.exports = orgRouter;
