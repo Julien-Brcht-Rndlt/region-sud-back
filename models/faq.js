23237918e0f06f28ee8570f032921e60e97e12c9
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-22 17:52:23 +0200

    adding some more behaviours and route services to faq

--- models/faq.js
+++ models/faq.js
@@ -1,15 +1,82 @@
 const connection = require('../db-config');
+const Joi = require('joi');
 
-const findAll = ({}) => {
-    return;
+const findAll = () => {
+  const sql = 'SELECT * FROM faq';
+    return connection.promise().query(sql)
+                    .then(([results]) => results);  
+}
+
+
+const find = (id) => {
+  const sql = 'SELECT * FROM faq WHERE id = ?';
+    connection.promise().query(sql, [id])
+                .then(([result]) => {
+                    if(!results.length) {
+                        return Promise.reject('RESSOURCE_NOT_FOUND');
+                    } else {
+                        return results;
+                    }
+                });
 };
 
-const find = (faqId) => {
-    return;
-}
+const create = ({ questionFaq , answerFaq  }) => {
+    const validationErrors = validation({ questionFaq, answerFaq });
+    if(validationErrors){
+        return Promise.reject('THE_QUESTION_ALREADY_EXISTS');
+    };
+    const sql = 'INSERT INTO faq (questionFaq, answerFaq) VALUE (?, ?)';
+    connection.promise().query(sql, [questionFaq, answerFaq])
+                        .then(([{insertId}]) => { insertId, questionFaq, answerFaq });
+};
 
+const modify = (id, {questionFaq, answerFaq}) => {
+    const validationErrors = validation({questionFaq, answerFaq});
+    if(validationErrors) {
+        return Promise.reject('CANT_CHANGE');
+    } find(id).then((faq) => {
+        let sql = 'SELECT * FROM faq WHERE questionFaq = ?';
+        connection.promise().query(sql, [questionFaq]).then(([results]) => {
+            if(results.lenght) {
+                if(results.filter((result) => result.questionFaq === questionFaq)) {
+                    return Promise.reject('QUESTION_DUPLICATE');
+                }
+            }
+            sql = 'UPDATE faq SET ? WHERE id = ?';
+            return connection.promise.query(sql, [{questionFaq, answerFaq}, id]);
+            }).catch((err) => {
+                if(err === 'RESOURCE_NOT_FOUND'){
+                    return Promise.reject(err);
+                }
+            });
+           }); 
+};
+
+const remove = (id) => {
+    find(id).then((faq) => {
+        const sql = 'DELETE FROM faq WHERE id = ?';
+        return connection.promise.query(sql, [id]);
+    }).catch((err) => {
+        if(err === 'RESSOURCE_NOT_FOUND'){
+            return Promise.reject(err);
+        }
+    });
+};
+
+const validation = ({ questionFaq, answerFaq }) => {
+    let validationErrors = null;
+    questionFaq && (validationProps['questionFaq'] = Joi.string().max().required());
+    answerFaq && (validationProps['answerFaq'] = Joi.string().max().required());
+    validationErrors = Joi.object(validationProps)
+                            .validate({ questionFaq, answerFaq }, { abortEarly: false })
+                            .error;
+            return validationErrors;
+}
 
 module.exports = {
-    findAll,
-    find,
-}
+  findAll,
+  find,
+  create,
+  modify,
+  remove,
+};
--- routes/faq.js
+++ routes/faq.js
@@ -1,15 +1,75 @@
-const faqRouter = require('express').Router();
+const FaqRouter = require('express').Router();
 const Faq = require('../models/faq');
 
-faqRouter.get('/', (req, res) => {
-    Faq.findAll().then((faq) => res.status(200).json(faq))
-        .catch((err) => res.status(500).send(`Error from Faq : ${err.message}`));
+/*Modif à voir sur les messages d'erreurs */
+
+FaqRouter.get('/emi/faq', (req, res) => {
+  Faq.findAll()
+    .then((faq) => res.status(200).json(faq))
+    .catch((err) => res.status(500).send(`Error retrieving faq: ${err.message}`));
 });
 
-faqRouter.get('/:id', (req, res) => {
-    Faq.find(faqId).then((faq) => res.status(200).json(faq))
-        .catch((err) => res.status(500).send(`Error from Faq ${faqId} : ${err.message} `));
+FaqRouter.get('/emi/faq/:id', (req, res) => {
+  const faqId = req.params.id;
+  Faq.find(faqId)
+    .then((faq) => res.status(200).json(faq))
+    .catch((err) => res.status(500).send(`Error retrieving faq (#${faqId}): ${err.message}`));
 });
 
+FaqRouter.post('/faq', (req, res) => {
+  const { questionFaq, answerFaq } = req.body;
+  Faq.create(faqId)
+    .then(([insertId]) => {
+      res.status(201).json({ id: insertId, ...req.body });
+    })
+    .catch((err) => {
+      if (err === 'DUPLICATE_QUESTION') {
+        res.status(409).json({ message: 'This question already exist' });
+      } else if (err === 'INVALIDE_DATA') {
+        res.status(422).json({ validationError });
+      } else {
+        res.status(500).send('Error saving content');
+      }
+    });
+});
+FaqRouter.put('/:id', (req, res) => {
+  const faqId = req.params.id;
+  Faq.modify(req.body)
+    .then((result) => {
+      const faqId = result.insertId;
+      res.status(200).json({ id: faqId, ...req.body });
+    })
+    .catch((err) => {
+      switch (err) {
+        case 'THE_QUESTION_ALREADY_EXISTS':
+          res.status(404).json({ message: `this ${faqId} can't change` });
+          break;
+        case 'CANT_CHANGE':
+          res.status(422).json({ message: 'the content has not been changed' });
+          break;
+        case 'QUESTION_DUPLICATE':
+          res.status(409).json({ message: 'the question exists' });
+          break;
+        default:
+          res.status(500).send(`Error with FAQ : ${err.message} `);
+          break;
+      }
+    });
+});
 
-module.export = faqRouter;
+FaqRouter.delete('/:id', (req, res) => {
+  const faqId = req.params.id;
+  Admin.remove(faqId)
+    .then((result) => {
+      const faqId = result.insertId;
+      res.status(200).json({ message: `this ${faqId} was deleted ` });
+    })
+    .catch((err) => {
+      if (err === 'RESOURCE_NOT_FOUND') {
+      } else {
+        res.status(500).send(` Error with FAQ : ${err.message} `);
+      }
+    });
+});
+
+module.exports = faqRouter;
