2bd47fe9624fca81a8fc3d4b179e5f7dcf37e616
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-26 00:22:07 +0200

    correcting some fk for findEventScores and findEventAnswers

--- models/eval.js
+++ models/eval.js
@@ -3,7 +3,7 @@ const connection = require('db-config');
 const findEventScores = (id) => {
     const sql = `SELECT FROM * FROM is_evaluated
                 JOIN theme ON is_evaluated.id_theme = theme.id
-                WHERE is_evaluated.id_event = ?
+                WHERE is_evaluated.id = ?
                 ORDER BY theme.id`;
 
     return connection.promise().query(sql, [id]).then(([results]) => results);
@@ -11,12 +11,12 @@ const findEventScores = (id) => {
 
 const findEventAnswers = (id) => {
     const sql = `SELECT FROM * FROM eval_answer
-                JOIN answer ON eval_answer.id_answer = answer.id
+                JOIN answer ON eval_answer.id = answer.id
                 LEFT JOIN related_to ON answer.id = related_to.id
                 RIGHT JOIN recommandation ON related_to.id_recommandation = recommandation.id
                 JOIN question ON question.id = answer.id_question
-                JOIN question ON theme.id = question.id_theme
-                WHERE eval_answer..id_event = ?
+                JOIN theme ON theme.id = question.id_theme
+                WHERE eval_answer.id = ?
                 ORDER BY theme.id, question.id, answer.id
                 ORDER BY theme.id`;
 
