a091b620de85fe88db95c61bff25c9490c320c67
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-25 19:37:32 +0200

    correct buildFunnel declaration

--- utils/funnel.js
+++ utils/funnel.js
@@ -1,7 +1,7 @@
 /**
  * utils function to build the Funnel json object from databases rows.
  */
-const buildFunnel = (rows) {
+const buildFunnel = (rows) => {
     let funnelJSON = null;
     return funnelJSON
 }
