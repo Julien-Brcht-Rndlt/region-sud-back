e22f15c11c84d05ecc85c22320c81b8467d69819
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-25 21:20:32 +0200

    findEvalScoresEvent behaviour +

--- models/eval.js
+++ models/eval.js
@@ -2,9 +2,8 @@ const connection = require('db-config');
 
 const findEventScores = (id) => {
     const sql = `SELECT FROM * FROM is_evaluated
-                JOIN event ON is_evaluated.id = event.id
                 JOIN theme ON is_evaluated.id_theme = theme.id
-                WHERE event.id = ?
+                WHERE is_evaluated.id_event = ?
                 ORDER BY theme.id`;
 
     return connection.promise().query(sql, [id]).then(([results]) => results);
