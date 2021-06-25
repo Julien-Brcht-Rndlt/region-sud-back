c519b9e14e0b80821fd5ce46f7d6c7224dfb9308
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-25 18:46:54 +0200

    adding some routers definitions

--- routes/routes-config.js
+++ routes/routes-config.js
@@ -1,9 +1,12 @@
 const orgRouter = require('./organizations');
+const eventRouter = require('./events');
 const faqRouter = require('./faq');
+const adminRouter = require('./admin');
 
 const setupRoutes = (app) => {
     // add routes
     app.use('/emi/organizations', orgRouter);
+    app.use('emi/events', eventRouter);
     app.use('emi/faq', faqRouter);
     app.use('emi/admin', adminRouter);
 }
