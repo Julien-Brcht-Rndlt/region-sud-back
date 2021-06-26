087b7cfb0dad3f5d23a79d842f81be4c86c1f5d9
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-26 19:24:30 +0200

    init buildFunnel func skeleton

---
+++ utils/funnel.js
@@ -0,0 +1,11 @@
+/**
+ * utils function to build the Funnel json content object from databases rows.
+ * @rows list of rows (resultset)
+ */
+ const buildFunnel = (rows) => {
+    
+ };
+
+ module.exports = {
+     buildFunnel,
+ }
