92719b9bec07d75be980937e450a34c257a32705
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-26 02:18:19 +0200

    create function to build eval scores event

--- utils/eval.js
+++ utils/eval.js
@@ -3,7 +3,18 @@
  * @rows list of rows (resultset)
  */
 const buildEvalScoresEvent = (rows) => {
-
+    const evalScores = {
+        event_id: rows[0].id,
+        event_title: rows[0].title,
+        themes: []
+    };
+    rows.forEach(row => {
+        evalScores.themes[row.theme_id] = {
+            theme_id: row.theme_id,
+            score: row.score,
+        }
+    });
+    return evalScores;
 };
 
 /**
