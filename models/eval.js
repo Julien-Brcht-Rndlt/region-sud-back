6940cb90562e689fd71bedd00c7ea6963a38ccff
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-07-04 22:19:56 +0200

    updates of SELECT join request for eval

--- models/eval.js
+++ models/eval.js
@@ -3,22 +3,27 @@ const connection = require('db-config');
 const findEventScores = (id) => {
     const sql = `SELECT FROM * FROM is_evaluated
                 JOIN theme ON is_evaluated.id_theme = theme.id
-                WHERE is_evaluated.id = ?
+                WHERE event.id = ?
                 ORDER BY theme.id`;
 
     return connection.promise().query(sql, [id]).then(([results]) => results);
 };
 
 const findEventAnswers = (id) => {
-    const sql = `SELECT FROM * FROM eval_answer
-                JOIN answer ON eval_answer.id = answer.id
-                LEFT JOIN related_to ON answer.id = related_to.id
-                RIGHT JOIN recommandation ON related_to.id_recommandation = recommandation.id
-                JOIN question ON question.id = answer.id_question
-                JOIN theme ON theme.id = question.id_theme
-                WHERE eval_answer.id = ?
-                ORDER BY theme.id, question.id, answer.id
-                ORDER BY theme.id`;
+  const sql = `SELECT theme.id as theme_id, theme.title as theme_title, theme.icon as theme_icon, 
+  question.id as question_id, question.title as question_title, id_theme,
+  answer.id as answer_id, answer.title as answer_title, weight, id_question,
+  related_to.id_answer as rel_to_id_answer,  related_to.id_recommandation as rel_to_id_recommandation, trigger_value, trigger_max,
+  recommandation.id as reco_id, recommandation.title as reco_label, recommandation.content as reco_content, recommandation.url as reco_url 
+  FROM event
+  LEFT JOIN eval_answer ON event.id = eval_answer.id_event
+  RIGHT JOIN answer ON eval_answer.id_answer = answer.id
+  LEFT JOIN related_to ON answer.id = related_to.id_answer
+  JOIN recommandation ON related_to.id_recommandation = recommandation.id
+  JOIN question ON question.id = answer.id_question
+  JOIN theme ON theme.id = question.id_theme
+  ORDER BY theme.id, question.id, answer.id, recommandation.id;
+  `;
 
     return connection.promise().query(sql, [id]).then(([results]) => results);
 }
