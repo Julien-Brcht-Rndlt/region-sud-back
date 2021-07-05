c404a6c520e3b8bc39123eb03943495256dc4c3c
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-07-05 09:53:24 +0200

    correcting evals file

--- routes/evals.js
+++ routes/evals.js
@@ -1,42 +1,40 @@
 const evalRouter = require('express').Router();
 const Eval = require('../models/eval');
-const Event = require('../modals/event');
-const evalScoresBuild = require('utils').evalScoresBuild;
+const Event = require('../models/event');
+const { evalScoresBuildJSON, buildEvalAnswersEventJSON } = require('../utils/eval').evalScoresBuildJSON;
 
 evalRouter.get('/events/:eventId/scores', (req, res) => {
-    const eventId = req.params.eventId;
-    Event.find(eventId)
-        .then((orgEvent) => {
-            orgEvent
-            Eval.findEventScores(eventId).then(([results]) => {
-                const evalScoresEvent = evalScoresBuild(results);
-                res.status(200).json(evalScoresEvent);
-            })
-            .catch((err) => {
-                res.status(500).json({ message: `Error while retrieving eval scores for event ${eventId} :${err.message}` });
-            });
-        })
+  const { eventId } = req.params.eventId;
+  Event.find(eventId)
+    .then(() => {
+      Eval.findEventScores(eventId).then(([results]) => {
+        const evalScoresEvent = evalScoresBuildJSON(results);
+        res.status(200).json(evalScoresEvent);
+      })
         .catch((err) => {
-            res.status(500).json({ message: `Error while retrieving event ${eventId} :${err.message}` });
+          res.status(500).json({ message: `Error while retrieving eval scores for event ${eventId} :${err.message}` });
         });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `Error while retrieving event ${eventId} :${err.message}` });
+    });
 });
 
 evalRouter.get('/events/:eventId/answers', (req, res) => {
-    const eventId = req.params.eventId;
-    Event.find(eventId)
-        .then((orgEvent) => {
-            orgEvent
-            Eval.findEventAnswers(eventId).then(([results]) => {
-                const evalAnswersEvent = buildEvalAnswersEvent(results);
-                res.status(200).json(evalScoresEvent);
-            })
-            .catch((err) => {
-                res.status(500).json({ message: `Error while retrieving eval answers for event ${eventId} :${err.message}` });
-            });
-        })
+  const { eventId } = req.params.eventId;
+  Event.find(eventId)
+    .then(() => {
+      Eval.findEventAnswers(eventId).then(([results]) => {
+        const evalAnswersEvent = buildEvalAnswersEventJSON(results);
+        res.status(200).json(evalAnswersEvent);
+      })
         .catch((err) => {
-            res.status(500).json({ message: `Error while retrieving event ${eventId} :${err.message}` });
+          res.status(500).json({ message: `Error while retrieving eval answers for event ${eventId} :${err.message}` });
         });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `Error while retrieving event ${eventId} :${err.message}` });
+    });
 });
 
 module.exports = evalRouter;
