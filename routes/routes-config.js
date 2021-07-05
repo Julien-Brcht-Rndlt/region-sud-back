15472e3e1631232b42d5375d1c96b3edfe9495a4
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-07-05 21:28:29 +0200

    changes to setup routes + app

--- routes/routes-config.js
+++ routes/routes-config.js
@@ -1,19 +1,19 @@
-/* const orgRouter = require('./organizations');
+const orgRouter = require('./organizations');
 const eventRouter = require('./events');
 const faqRouter = require('./faq');
 const adminRouter = require('./admins');
-const themeRouter = require('./themes'); */
+const themeRouter = require('./themes');
 const funnelRouter = require('./funnels');
-/* const evalRouter = require('./evals'); */
+const evalRouter = require('./evals');
 
 const setupRoutes = (app) => {
-  /* app.use('/emi/organizations', orgRouter);
-  app.use('emi/events', eventRouter);
-  app.use('emi/faq', faqRouter);
-  app.use('emi/admin', adminRouter);
-  app.use('/emi/themes', themeRouter); */
+  app.use('/emi/organizations', orgRouter);
+  app.use('/emi/events', eventRouter);
+  app.use('/emi/faq', faqRouter);
+  app.use('/emi/admins', adminRouter);
+  app.use('/emi/themes', themeRouter);
   app.use('/emi/funnels', funnelRouter);
-/*   app.use('/evals/1', evalRouter); */
+  app.use('/evals/1', evalRouter);
 };
 
 module.exports = {
