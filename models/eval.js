1b35a4e586b71eb1ea25e1e6e2b59d8983f7c782
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-07-05 09:26:30 +0200

    removed .jsx files :/

--- models/eval.js
+++ models/eval.js
@@ -3,7 +3,7 @@ const connection = require('db-config');
 const findEventScores = (id) => {
     const sql = `SELECT FROM * FROM is_evaluated
                 JOIN theme ON is_evaluated.id_theme = theme.id
-                WHERE event.id = ?
+                WHERE is_evaluated.id_event = ?
                 ORDER BY theme.id`;
 
     return connection.promise().query(sql, [id]).then(([results]) => results);
