9795810b733b68281c8c3032ee34ba7a1acb48ff
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-07-05 21:25:27 +0200

    improvement of all get routes for evals

--- models/eval.js
+++ models/eval.js
@@ -1,12 +1,12 @@
-const connection = require('db-config');
+const connection = require('../db-config');
 
 const findEventScores = (id) => {
-    const sql = `SELECT FROM * FROM is_evaluated
+  const sql = `SELECT FROM * FROM is_evaluated
                 JOIN theme ON is_evaluated.id_theme = theme.id
                 WHERE is_evaluated.id_event = ?
                 ORDER BY theme.id`;
 
-    return connection.promise().query(sql, [id]).then(([results]) => results);
+  return connection.promise().query(sql, [id]).then(([results]) => results);
 };
 
 const findEventAnswers = (id) => {
@@ -25,10 +25,10 @@ const findEventAnswers = (id) => {
   ORDER BY theme.id, question.id, answer.id, recommandation.id;
   `;
 
-    return connection.promise().query(sql, [id]).then(([results]) => results);
-}
+  return connection.promise().query(sql, [id]).then(([results]) => results);
+};
 
 module.exports = {
-    findEventScores,
-    findEventAnswers,
-}
+  findEventScores,
+  findEventAnswers,
+};
--- routes/evals.js
+++ routes/evals.js
@@ -1,40 +1,57 @@
 const evalRouter = require('express').Router();
 const Eval = require('../models/eval');
 const Event = require('../models/event');
-const { evalScoresBuildJSON, buildEvalAnswersEventJSON } = require('../utils/eval').evalScoresBuildJSON;
+const { RESOURCE_NOT_FOUND } = require('../constants');
+const { evalScoresBuildJSON, buildEvalAnswersEventJSON } = require('../utils/eval');
 
 evalRouter.get('/events/:eventId/scores', (req, res) => {
   const { eventId } = req.params.eventId;
   Event.find(eventId)
-    .then(() => {
-      Eval.findEventScores(eventId).then(([results]) => {
-        const evalScoresEvent = evalScoresBuildJSON(results);
-        res.status(200).json(evalScoresEvent);
-      })
-        .catch((err) => {
-          res.status(500).json({ message: `Error while retrieving eval scores for event ${eventId} :${err.message}` });
-        });
+    .then((event) => {
+      if (!event) {
+        return Promise.reject(new Error(RESOURCE_NOT_FOUND));
+      }
+      return Eval.findEventScores(eventId);
+    })
+    .then(([results]) => {
+      const evalScoresEvent = evalScoresBuildJSON(results);
+      res.status(200).json(evalScoresEvent);
     })
     .catch((err) => {
-      res.status(500).json({ message: `Error while retrieving event ${eventId} :${err.message}` });
+      if (err === RESOURCE_NOT_FOUND) {
+        res.status(404).json({ message: `Resource event ${eventId} not found!` });
+      } else {
+        res.status(500).json({ message: `Error while retrieving event ${eventId} :${err.message}` });
+      }
     });
 });
 
 evalRouter.get('/events/:eventId/answers', (req, res) => {
   const { eventId } = req.params.eventId;
   Event.find(eventId)
-    .then(() => {
-      Eval.findEventAnswers(eventId).then(([results]) => {
-        const evalAnswersEvent = buildEvalAnswersEventJSON(results);
-        res.status(200).json(evalAnswersEvent);
-      })
-        .catch((err) => {
-          res.status(500).json({ message: `Error while retrieving eval answers for event ${eventId} :${err.message}` });
-        });
+    .then((event) => {
+      if (!event) {
+        return Promise.reject(new Error(RESOURCE_NOT_FOUND));
+      }
+      return Eval.findEventAnswers(eventId);
+    })
+    .then(([results]) => {
+      const evalAnswersEvent = buildEvalAnswersEventJSON(results);
+      res.status(200).json(evalAnswersEvent);
     })
     .catch((err) => {
-      res.status(500).json({ message: `Error while retrieving event ${eventId} :${err.message}` });
+      if (err === RESOURCE_NOT_FOUND) {
+        res.status(404).json({ message: `Resource event ${eventId} not found!` });
+      } else {
+        res.status(500).json({ message: `Error while retrieving event ${eventId} :${err.message}` });
+      }
     });
 });
+
+/* evalRouter.post('/events/:eventId/scores', (req, res) => {
+  const { eventId } = req.params.eventId;
+
+
+}); */
 
 module.exports = evalRouter;
