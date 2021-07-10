3302c087bbe7c316cdd11c8ccc9c3d6d9b57829e
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-07-10 19:22:12 +0200

    correcting routes code

--- routes/recos.js
+++ routes/recos.js
@@ -4,19 +4,32 @@ const { DUPLICATE_TITLE, RESOURCE_NOT_FOUND } = require('../constants');
 
 recoRouter.get('/', (req, res) => {
   Reco.findAll()
-    .then(([results]) => res.status(200).json(results))
+    .then((results) => res.status(200).json(results))
     .catch((err) => res.status(500).json({ message: `Error while retrieving recommandations : ${err.message}` }));
 });
 
 recoRouter.get('/:id', (req, res) => {
   const { id } = req.params;
+  console.log(`get ${id}`);
   Reco.find(id)
+    .then((reco) => {
+      if (!reco) {
+        return Promise.reject(new Error(RESOURCE_NOT_FOUND));
+      }
+      return reco;
+    })
     .then((reco) => res.status(200).json(reco))
-    .catch((err) => res.status(500).json({ message: `Error while retrieving recommandation #${id} : ${err.message}` }));
+    .catch((err) => {
+      if (err.message === RESOURCE_NOT_FOUND) {
+        res.status(404).json({ message: `Resource recommandation ${id} not found!` });
+      } else {
+        res.status(500).json({ message: `Error while retrieving recommandation #${id} : ${err.message}` });
+      }
+    });
 });
 
 recoRouter.post('/', (req, res) => {
-  Reco.findByTitle()
+  Reco.findByTitle(req.body.title)
     .then((results) => {
       if (results && results.length) {
         return Promise.reject(new Error(DUPLICATE_TITLE));
@@ -34,35 +47,37 @@ recoRouter.post('/', (req, res) => {
 });
 
 recoRouter.put('/:id', (req, res) => {
-  const { recoId } = req.params;
-  Reco.find(recoId)
+  const { id } = req.params;
+  Reco.find(id)
     .then((reco) => {
       if (!reco) {
         return Promise.reject(new Error(RESOURCE_NOT_FOUND));
       }
       return reco;
     })
-    .then((reco) => Reco.modify(recoId, reco, req.body))
+    .then((reco) => Reco.modify(id, reco, req.body))
     .then((updatedReco) => res.status(200).json(updatedReco))
     .catch((err) => {
       if (err.message === RESOURCE_NOT_FOUND) {
-        res.status(404).json({ message: `Resource organization ${recoId} not found!` });
+        res.status(404).json({ message: `Resource organization ${id} not found!` });
       } else {
-        res.status(500).json({ message: `Error while modifying resource recommandation #${recoId}: ${err.message}` });
+        res.status(500).json({ message: `Error while modifying resource recommandation #${id}: ${err.message}` });
       }
     });
 });
 
 recoRouter.delete('/:id', (req, res) => {
-  const { recoId } = req.params;
-  Reco.remove(recoId)
+  const { id } = req.params;
+  Reco.remove(id)
     .then((result) => {
-      console.log(result);
+      console.log(result.affectedRows);
       if (result.affectedRows) {
-        res.status(204).json({ message: `Resource recommandation ${recoId} has been definitely removed` });
+        res.status(200).json({ message: `Resource recommandation ${id} has been definitely removed` });
+      } else {
+        res.status(404).json({ message: `Resource organization ${id} not found!` });
       }
     })
-    .catch((err) => res.status(500).json({ message: `Error while removing resource recommandation #${recoId}: ${err.message}` }));
+    .catch((err) => res.status(500).json({ message: `Error while removing resource recommandation #${id}: ${err.message}` }));
 });
 
 module.exports = recoRouter;
