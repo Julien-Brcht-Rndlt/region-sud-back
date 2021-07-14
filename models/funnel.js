bc8523e776769090f72576b7fda393d5a422b198
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-07-15 00:09:53 +0200

    adding load of recos to JSON funnel object

--- models/funnel.js
+++ models/funnel.js
@@ -5,7 +5,7 @@ const find = () => {
   SELECT theme.id as theme_id, theme.title as theme_title, theme.icon as theme_icon, 
   question.id as question_id, question.title as question_title, id_theme,
   answer.id as answer_id, answer.title as answer_title, answer.weight as weight, answer.id_question as answ_id_question , answer.id_answer_type as answ_id_answer_type,
-  answer_type.id as type_id, answer_type.title as type, recommandation.title as reco_title, recommandation.content as reco_content, recommandation.url as reco_url
+  answer_type.id as type_id, answer_type.title as type, recommandation.id as reco_id, recommandation.title as reco_title, recommandation.content as reco_content, recommandation.url as reco_url, trigger_value, trigger_max
   FROM theme
   JOIN question ON theme.id = question.id_theme
   JOIN answer ON question.id = answer.id_question
